aa26fa7b5e482226807215669f8e8dff17b0752d
Cody Gipson <CGipson06@yahoo.com>
2015-06-15 05:51:40 -0500

    added bubble_sort_by

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,6 +1,6 @@
 def bubble_sort(list)
   
-  (list.size-1).times do 
+  (list.size-1).times do #This is the maximum solution length
   a,b=0,1
    (list.size-1).times do 
      if list[a]>list[b]
@@ -9,11 +9,31 @@ def bubble_sort(list)
 	  a+=1
 	  b+=1
 	end
-	print list
    end
-  
+  print list
 end
 		
-bubble_sort([5,4,3,2,1,9,8,7,6])
-	
-	
+bubble_sort([5,4,3,2,1])
+
+
+
+#Bubble_sort_by will take a list AND a block, using the block to process the sorting
+def bubble_sort_by(list)
+  (list.size-1).times do 
+  a,b=0,1
+  (list.size-1).times do 
+     if yield(list[a],list[b]) < 0
+		list[a],list[b]=list[b],list[a]
+	  end
+	  a+=1
+	  b+=1
+	end
+   end
+  print list
+end
+   
+
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  right.length - left.length
+end
